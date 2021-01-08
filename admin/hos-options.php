7eac5c3acf47f7bb904cb6b411ed6a14441e5e17
crimson_lotus <45713563+crimson-lotus@users.noreply.github.com>
2021-01-08 22:12:31 +0530

    added admin options/operations skleton

---
+++ admin/hos-options.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Core Bootstrap Code -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <!-- Custom Styles -->
+        <link rel="stylesheet" href="">
+    </head>
+    <body class="text-center">
+        
+        <div style="margin: 9% 0 1.5%;">
+            <h4>What would you like to change?</h4>
+        </div>
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 p-3">
+                    <a href=""><button type="button" class="btn btn-outline-primary">Add Into Hospital Table</button></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 p-3">
+                    <a href=""><button type="button" class="btn btn-outline-primary">Delete from Hospital Table</button></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 p-3">
+                    <a href=""><button type="button" class="btn btn-outline-primary">Update Hospital Table</button></a>
+                </div>
+            </div>
+        </div>
+        <footer class="container-fluid text-center">
+            <div class="col-12">
+              <div class="p-3"><a href="admin-page.html"><button class="btn btn-details btn-primary">Back To Dashboard</button></a></div>
+            </div>
+        </footer>
+        <!-- <script src="" async defer></script> -->
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    </body>
+</html>
